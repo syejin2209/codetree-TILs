ace9fb6854fcea80d9032856d6fbb3a7fa805894
syejin2209 <tamamavv1009@gmail.com>
2024-07-31 17:54:39 +0900

    Title: 100 도달하기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240731/100 도달하기/reach-100.py
+++ 240731/100 도달하기/reach-100.py
@@ -1,10 +1,12 @@
 n = int(input())
 arr = [1, n]
+cnt = 1
 
 while True:
-    for i in range(1, len(arr)):
-        arr.append(arr[len(arr) - 1] + arr[len(arr) - 2])
-    if arr[len(arr) - 1] >= 100:
+    cnt += 1
+    arr.append(arr[cnt - 1] + arr[cnt - 2])
+    if arr[cnt] > 100:
         break
 
-print(*arr)
+for elem in arr:
+    print(elem, end=" ")
