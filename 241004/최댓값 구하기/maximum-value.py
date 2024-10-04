73db6fa5c5c4d460dcb2be384efa75736226f37c
syejin2209 <tamamavv1009@gmail.com>
2024-10-04 10:39:59 +0900

    Title: 최댓값 구하기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241004/최댓값 구하기/maximum-value.py
@@ -0,0 +1,14 @@
+inp = input()
+arr = inp.split()
+a, b, c = int(arr[0]), int(arr[1]), int(arr[2])
+
+if a >= b:
+    if a >= c:
+        print(a)
+    else:
+        print(c)
+else:
+    if c >= b:
+        print(c)
+    else:
+        print(b)
