1c46cb75743e69e9c0ac3fc4d5585a2c3839f666
syejin2209 <tamamavv1009@gmail.com>
2024-10-06 02:12:45 +0900

    Title: 1까지 나누기, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241006/1까지 나누기/divide-by-1.py
+++ 241006/1까지 나누기/divide-by-1.py
@@ -2,8 +2,10 @@ n = int(input())
 cnt = 0
 
 for i in range(1, n + 1):
-    if n % i == 0:
+    if n / i > 0:
         cnt += 1
-    if n / i <= 1:
+        n = n // i
+        continue
+    if n // i <= 1:
         break
-print(cnt - 1)
+print(cnt)
