76d684e1511e81f8c7a3553b299e3d713817d358
syejin2209 <tamamavv1009@gmail.com>
2024-10-07 19:21:31 +0900

    Title: 별표 출력하기 4, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241007/별표 출력하기 4/print-star-4.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for i in range(n - 1, -1, -1):
+    for j in range(i + 1):
+        print('*', end=' ')
+    print()
+for i in range(1, n):
+    for j in range(i + 1):
+        print('*', end=' ')
+    print()
